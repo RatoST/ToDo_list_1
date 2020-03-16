c116d0f4355dbd9ef8d059edb285e6f4c325baba
Rato <47523773+RatoST@users.noreply.github.com>
2020-03-16 11:33:45 +0100

    ToDo list initial with two separate lists

---
+++ app.js
@@ -0,0 +1,63 @@
+// jshint esversion:6
+//create express and body-parser
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+//create array of items so when rendering starts item wont be empty
+let items = ["Wake up", "Make & drink juice", "Make & drink coffe"];
+let workItems =[];
+
+//use ejs
+app.set("view engine", "ejs");
+
+// use bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// use folder public for css, js etc through express
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  //Render all items from one place
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+// logic for combine of two lists
+  if (req.body.list ==="Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle:"Work List", newListItems:workItems});
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
