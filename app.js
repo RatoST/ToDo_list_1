c650869fc2f5b16331a9f5599a585ae1e62c08a9
Rato <47523773+RatoST@users.noreply.github.com>
2020-03-16 13:40:20 +0100

    Final list for home and work tasks.

--- app.js
+++ app.js
@@ -2,66 +2,67 @@
 //create express and body-parser
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 //create array of items so when rendering starts item wont be empty
-let items = ["Wake up", "Make & drink juice", "Make & drink coffe"];
-let workItems =[];
+const items = ["Wake up", "Make & drink juice", "Make & drink coffe"];
+const workItems = [];
 
 //use ejs
 app.set("view engine", "ejs");
 
 // use bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // use folder public for css, js etc through express
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  //call function through variable day
+  const day = date.getDate();
 
   //Render all items from one place
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 
 });
 
 app.post("/", function(req, res) {
 
-  let item = req.body.newItem;
-// logic for combine of two lists
-  if (req.body.list ==="Work") {
+  const item = req.body.newItem;
+  // logic for combine of two lists
+  if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-  }else{
+  } else {
     items.push(item);
     res.redirect("/");
   }
 });
 
-app.get("/work", function(req, res){
-  res.render("list", {listTitle:"Work List", newListItems:workItems});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.post("/work", function(req, res){
-  let item = req.body.newItem;
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
